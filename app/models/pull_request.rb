c7dc820a81334125de5fff228f1b2f4759788e9e
David Young <david.young@junipereducation.org>
2023-03-14 17:00:06 +0000

    Added status to Pull Request

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -3,4 +3,14 @@ class PullRequest < ApplicationRecord
   has_many :comments, -> {where(type: nil)} #returns only type nil (regular Comment)
   has_many :change_requests
   has_many :approvals
+
+  def status
+    if approvals.any?
+      :approved
+    elsif change_requests.any?
+      :change
+    else
+      :awaiting_approval
+    end
+  end
 end
